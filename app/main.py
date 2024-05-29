231ed15484b1685928d3202a88e9ccbb0ce7f24a
Ford <vbivol394@gmail.com>
2024-05-29 15:36:58 +0300

    pass stage

--- app/main.py
+++ app/main.py
@@ -4,11 +4,18 @@ import sys
 def main():
 
 
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
-    comand = input()
 
-    print(f"{comand}: command not found")
+    
+    valid_comands = {}
+
+    while True:
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
+        user_comands = input()
+        if user_comands not in valid_comands:
+            print(f"{user_comands}: command not found")
+
+
 
 
 if __name__ == "__main__":
