120c8a0e871005c473ef3a0c899d24169e40f915
Ford <vbivol394@gmail.com>
2024-05-29 16:29:40 +0300

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,26 +1,20 @@
 import sys
 
-
 def main():
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
-        if not user_comands:
-            continue
+        user_comands = input().strip()
+
+        if user_comands == 'exit 0':
+            sys.exit(0)
         
-        if user_comands := input().strip():
-
-            if user_comands == 'exit 0':
-                sys.exit(0)
-            elif user_comands.startswith('echo '):
-                # Extrage textul după 'echo ' și îl afișează
-                echo_text = user_comands[5:]
-                print(echo_text)  
-            else:
-                print(f"{user_comands}: command not found")
-
-
-
+        elif user_comands.startswith('echo '):
+            echo_text = user_comands[5:]
+            print(echo_text)
+        
+        else:
+            print(f"{user_comands}: command not found")
 
 if __name__ == "__main__":
     main()
