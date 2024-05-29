78fec5158f3e046899a20651dddb3a17ca330b29
Ford <vbivol394@gmail.com>
2024-05-29 15:41:19 +0300

    pass stage

--- app/main.py
+++ app/main.py
@@ -12,6 +12,9 @@ def main():
         sys.stdout.write("$ ")
         sys.stdout.flush()
         user_comands = input()
+        if user_comands == 'exit':
+            sys.exit()
+            
         if user_comands not in valid_comands:
             print(f"{user_comands}: command not found")
 
