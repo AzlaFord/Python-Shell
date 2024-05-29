b444ba2624b3816f3c37cd1e656ad94e426e8eac
Ford <vbivol394@gmail.com>
2024-05-29 16:26:30 +0300

    pass stage

--- app/main.py
+++ app/main.py
@@ -5,11 +5,16 @@ def main():
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
+        if not user_comands:
+            continue
+        
         if user_comands := input().strip():
 
             if user_comands == 'exit 0':
                 sys.exit(0)
-                
+            elif user_comands.startswith('echo '):
+                echo_command = user_comands[5:]
+                print(echo_command)    
             else:
                 print(f"{user_comands}: command not found")
 
