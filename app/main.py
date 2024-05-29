ad4fe95614987e5dbf08989a9b5edbeb5c87a3df
Ford <vbivol394@gmail.com>
2024-05-29 15:42:32 +0300

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,25 +1,22 @@
 import sys
 
-
 def main():
-
-
-
-    
     valid_comands = {}
 
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
-        user_comands = input()
-        if user_comands == 'exit':
-            sys.exit()
-            
-        if user_comands not in valid_comands:
-            print(f"{user_comands}: command not found")
-
-
-
+        user_comands = input().strip().split()  # Citește și împarte comanda în cuvinte
+        
+        if len(user_comands) > 0 and user_comands[0] == "exit":
+            if len(user_comands) == 2 and user_comands[1] == "0":
+                print("Exiting the program with status 0. Goodbye!")
+                sys.exit(0)  # Termină programul cu codul de stare 0
+            else:
+                print(f"{user_comands}: invalid exit command")
+        
+        if user_comands[0] not in valid_comands:
+            print(f"{user_comands[0]}: command not found")
 
 if __name__ == "__main__":
     main()
